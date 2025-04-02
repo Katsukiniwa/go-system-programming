a2a3e9281603ab3325ac1d8eacffb4d1424be6cd
Katsukiniwa <katsukiniwa0217@gmail.com>
2025-04-02 10:05:12 +0900

    chore: 脱線

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # go-system-programming
+
+[Goなら分かるシステムプログラミング](https://www.lambdanote.com/products/go-2)を勉強するリポジトリ
+
+## 参考飼料
+
+[【Go】このコードの意味が分かれば、ゴルーチンの基本は大丈夫](https://zenn.dev/farstep/articles/f712e05bd6ff9d)
--- main.go
+++ main.go
@@ -3,33 +3,92 @@ package main
 import (
 	"fmt"
 	"math"
+	"sync"
+	"time"
 )
 
-func primeNumber() chan int {
+func PrimeNumber() chan int {
 	result := make(chan int)
 	go func() {
 		result <- 2
+
 		for i := 3; i < 100; i += 1 {
 			l := int(math.Sqrt(float64(i)))
 			found := false
+
 			for j := 3; j < l+1; j += 2 {
 				if i%j == 0 {
 					found = true
+
 					break
 				}
 			}
+
 			if !found {
 				result <- i
 			}
 		}
+
 		close(result)
 	}()
+
 	return result
 }
 
 func main() {
-	pn := primeNumber()
+	pn := PrimeNumber()
 	for n := range pn {
 		fmt.Println(n)
 	}
+
+	start := time.Now()
+	fav()
+	fmt.Println("took: ", time.Since(start))
+}
+
+func fav() {
+	post := fetchPost()
+
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+	// channel の初期化
+	// 2個のバッファを持った channel を作成
+	resChan := make(chan any, 2)
+
+	go fetchPostLikes(post, resChan, &wg)
+	go fetchPostComments(post, resChan, &wg)
+
+	wg.Wait()
+
+	// resChan channel への送信を終了し channel を閉じる
+	close(resChan)
+
+	// channel が閉じられるまでループする
+	for res := range resChan {
+		fmt.Println("res: ", res)
+	}
+}
+
+// 投稿を一件取得する関数.
+func fetchPost() string {
+	time.Sleep(time.Millisecond * 50)
+
+	return "What programming languages do you prefer?"
+}
+
+// 投稿に紐づいたいいね数を取得する関数.
+func fetchPostLikes(post string, reschan chan any, wg *sync.WaitGroup) {
+	time.Sleep(time.Millisecond * 50)
+
+	reschan <- 10
+	wg.Done()
+}
+
+// 投稿に紐づいたコメントを全て取得する関数.
+func fetchPostComments(post string, reschan chan any, wg *sync.WaitGroup) {
+	time.Sleep(time.Millisecond * 100)
+
+	reschan <- []string{"Golang", "Java", "Rust"}
+	wg.Done()
 }
